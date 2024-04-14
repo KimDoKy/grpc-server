b54286d9fca150112f61ad134908d787116eb014
Dokyoung Kim <dkkim@tecace.com>
2024-04-14 22:22:10 +0900

    business logic of PasetoToken

--- gRPC/paseto/paseto.go
+++ gRPC/paseto/paseto.go
@@ -1,6 +1,7 @@
 package paseto
 
 import (
+	"crypto/rand"
 	"grpc-server/config"
 	auth "grpc-server/gRPC/proto"
 
@@ -13,16 +14,16 @@ type PasetoMaker struct {
 }
 
 func NewPasetoMaker(cfg *config.Config) *PasetoMaker {
-	return &PasetoMaker{
-		Pt:  paseto.NewV2(),
-		Key: []byte(cfg.Paseto.Key),
-	}
+	return &PasetoMaker{Pt: paseto.NewV2(), Key: []byte(cfg.Paseto.Key)}
 }
 
 func (m *PasetoMaker) CreateNewToken(auth *auth.AuthData) (string, error) {
-	return "", nil
+	randomBytes := make([]byte, 16)
+	rand.Read(randomBytes)
+	return m.Pt.Encrypt(m.Key, auth, randomBytes)
 }
 
 func (m *PasetoMaker) VerifyToken(token string) error {
-	return nil
+	var auth *auth.AuthData
+	return m.Pt.Decrypt(token, m.Key, auth, nil)
 }
