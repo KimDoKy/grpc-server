81bae2913f69e149334bad618148ad6a3adc3185
Dokyoung Kim <dkkim@tecace.com>
2024-04-14 23:03:04 +0900

    Add method to implement interface of gRPC server

--- gRPC/server/server.go
+++ gRPC/server/server.go
@@ -1,19 +1,22 @@
 package server
 
 import (
+	"context"
 	"grpc-server/config"
 	"grpc-server/gRPC/paseto"
 	auth "grpc-server/gRPC/proto"
 	"log"
 	"net"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 type GRPCServer struct {
+	auth.AuthServiceServer
 	pasetoMaker    *paseto.PasetoMaker
-	tokenVerifyMap map[string]auth.AuthData
+	tokenVerifyMap map[string]*auth.AuthData
 }
 
 func NewGRPCServer(cfg *config.Config) error {
@@ -22,10 +25,10 @@ func NewGRPCServer(cfg *config.Config) error {
 	} else {
 		server := grpc.NewServer([]grpc.ServerOption{}...)
 
-		// auth.RegisterAuthServiceServer(server, &GRPCServer{
-		// 	pasetoMaker:    paseto.NewPasetoMaker(cfg),
-		// 	tokenVerifyMap: make(map[string]*auth.AuthData),
-		// })
+		auth.RegisterAuthServiceServer(server, &GRPCServer{
+			pasetoMaker:    paseto.NewPasetoMaker(cfg),
+			tokenVerifyMap: make(map[string]*auth.AuthData),
+		})
 
 		reflection.Register(server)
 
@@ -38,4 +41,32 @@ func NewGRPCServer(cfg *config.Config) error {
 
 	}
 	return nil
+}
+
+func (s *GRPCServer) CreateAuth(_ context.Context, req *auth.CreateTokenReq) (*auth.CreateTokenRes, error) {
+	data := req.Auth
+	token := data.Token
+	s.tokenVerifyMap[token] = data
+	return &auth.CreateTokenRes{Auth: data}, nil
+}
+
+func (s *GRPCServer) VerifyAuth(_ context.Context, req *auth.VerifyTokenReq) (*auth.VerifyTokenRes, error) {
+	token := req.Token
+	res := &auth.VerifyTokenRes{V: &auth.Verity{
+		Auth: nil,
+	}}
+	if authData, ok := s.tokenVerifyMap[token]; !ok {
+		res.V.Status = auth.ResponseType_FAILED
+	} else if authData.ExpireDate < time.Now().Unix() {
+		delete(s.tokenVerifyMap, token)
+		res.V.Status = auth.ResponseType_EXPIRED_DATE
+	} else {
+		res.V.Status = auth.ResponseType_SUCCESS
+	}
+
+	return res, nil
+}
+
+func (s *GRPCServer) mustEmbedUnimplementedAuthServiceServer() {
+	panic("implement me")
 }
